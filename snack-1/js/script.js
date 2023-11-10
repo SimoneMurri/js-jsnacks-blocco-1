e7ae3acac561751a36427490785daa0e9e737b0c
Simone Murri <simonemurri701@gmail.com>
2023-11-10 11:55:09 +0100

    inserito console.log nell'if

--- snack-1/js/script.js
+++ snack-1/js/script.js
@@ -2,8 +2,8 @@ const firstAge = prompt('type your age');
 const secondAge = prompt('type your age');
 
 if (firstAge > secondAge){
-
+   console.log(firstAge);
 } else if (secondAge > firstAge) {
-
+   console.log(secondAge);
 }
    
